78c5b5c67aee3d3817d6ca93953f673c622d53c2
ngthecoder <ngoto0208@gmail.com>
2024-04-18 00:32:21 -0400

    Updated power_function

--- lambda/power_fucntion.py
+++ lambda/power_fucntion.py
@@ -1,9 +1,22 @@
 import json
 import math
+import boto3
+from time import gmtime, strftime
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('DynamoDBTable')
+now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
 
 def lambda_handler(event, context):
     mathResult = math.pow(int(event['base']), int(event['exponent']))
-    
+
+    response = table.put_item(
+        Item={
+            'ID': str(mathResult),
+            'LatestGreetingTime':now
+        }
+    )
+
     return {
         'statusCode': 200,
         'body': json.dumps('Your result is ' + str(mathResult))
