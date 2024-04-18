b955512a96069e2d9e00645e8e736f0b84237f75
ngthecoder <ngoto0208@gmail.com>
2024-04-18 00:45:58 -0400

    Added API call to calculate power in index.js

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const calculatePower = () => {
     const baseValue = document.getElementById('base').value;
     const exponentValue = document.getElementById('exponent').value;
+    
     console.log(`Calculating power for base: ${baseValue}, exponent: ${exponentValue}`);
+    
+    var myHeaders = new Headers();
+    
+    myHeaders.append("Content-Type", "application/json");
+    
+    var raw = JSON.stringify(
+        {
+            "base":baseValue,
+            "exponent":exponentValue
+        }
+    );
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    
+    fetch("https://onn2zqvh3m.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
+    .then(response => response.text())
+    .then(result => alert(JSON.parse(result).body))
+    .catch(error => console.log('error', error));
 };
 
 document.getElementById("calculateBtn").addEventListener("click", calculatePower);
