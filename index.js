bed3bd4c60a27c42721d55414b0ae50d9889bd63
ngthecoder <ngoto0208@gmail.com>
2024-04-19 04:52:27 -0400

    Updated the function

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
 const calculatePower = () => {
     const baseValue = document.getElementById('base').value;
     const exponentValue = document.getElementById('exponent').value;
-    
-    console.log(`Calculating power for base: ${baseValue}, exponent: ${exponentValue}`);
-    
     var myHeaders = new Headers();
-    
-    myHeaders = {
-        "Content-Type":"application/json",
-        "Access-Control-Allow-Origin":"*",
-        "Access-Control-Allow-Methods":"POST,PATCH,OPTIONS"
-    };
-    
-    var raw = JSON.stringify(
-        {
-            "base":baseValue,
-            "exponent":exponentValue
-        }
-    );
-
+    myHeaders.append("Content-Type", "application/json");
+    var raw = JSON.stringify({"base":baseValue,"exponent":exponentValue});
     var requestOptions = {
         method: 'POST',
         headers: myHeaders,
         body: raw,
         redirect: 'follow'
     };
-    
     fetch("https://s7ib7i5a66.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
     .then(response => response.text())
     .then(result => alert(JSON.parse(result).body))
